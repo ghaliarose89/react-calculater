34d4b47c63752177f8d4e767108c3d03e640e22e
unknown <ghaliarose89@gmail.com>
2022-01-29 12:07:46 -0600

    Bulding components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
 import './App.css';
+import MainBox from './components/MainBox';
+import  Display  from "./components/Display"
+import ButtonBox  from "./components/ButtonBox"
+import Button from './components/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [cal, setCal] = useState(
+    {
+        sign : '',
+        number:0,
+        res :0
+  
+    }
+  )
+  const btnArr = [
+    ["(", ")", "%", "/"],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".","="],["AC","^"],
+   
+  ];
+
+  const btnClickHandler = (e) => {
+   
+    switch (e) {
+      case 'AC':
+        //resetClickHandler();
+        break;
+      
+        default:
+    console.log(`Sorry, we are out of `);
+    }
+     
+    
+  }
+  
+
+    return (
+      <MainBox>
+        <Display value={cal.number?cal.number:cal.res} />
+        <ButtonBox>
+        {
+          btnArr.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={()=>btnClickHandler(btn)}
+              />
+            );
+          })
+        }
+             
+         
+     
+          
+        </ButtonBox>
+      </MainBox>
+    );
+ 
 }
 
 export default App;
+  
